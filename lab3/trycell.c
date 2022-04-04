ec171be1f8c5c82122374ee6bd11d7f309b75ef9
Manuel Gutierrez Jr <meny_nexi@outlook.com>
2022-04-03 21:21:55 -0600

    Add files via upload

---
+++ lab3/trycell.c
@@ -0,0 +1,36 @@
+extern unsigned int returnsp();
+
+int trycell(int *x, int pos)
+{
+    int row = pos / 9;
+    int col = pos % 9;
+    int i, j, used = 0;
+    unsigned int address = returnsp();
+    printf("going into the stack trace: %p\n",address);
+
+    if (pos == 81) return 1;
+    if (x[pos]) return trycell(x, pos + 1);
+
+    for (i = 0; i < 9; i++)
+        used |= 1 << (x[i * 9 + col] - 1);
+
+    for (j = 0; j < 9; j++)
+        used |= 1 << (x[row * 9 + j] - 1);
+
+    row = row / 3 * 3;
+    col = col / 3 * 3;
+    for (i = row; i < row + 3; i++)
+        for (j = col; j < col + 3; j++)
+            used |= 1 << (x[i * 9 + j] - 1);
+
+    for (x[pos] = 1; x[pos] <= 9; x[pos]++, used >>= 1)
+        if (!(used & 1) && trycell(x, pos + 1)) return 1;
+
+   printf("end of the the stack trace: %p\n",address);
+
+    x[pos] = 0;
+    return 0;
+}
+
+
+
